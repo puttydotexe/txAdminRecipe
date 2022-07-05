21452f16f1d61d1ea72eb49f772d0588e0bdf674
Joshua R <puttydotexe@users.noreply.github.com>
2022-07-05 04:19:35 +0100

    Update qbcore.sql

--- qbcore.sql
+++ qbcore.sql
@@ -1,3 +1,6 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
 CREATE TABLE IF NOT EXISTS `apartments` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
